48bad50f3c3be3f35809289602fac63ba3d6dc1c
dokdo <132822988+onepythonlove@users.noreply.github.com>
2025-02-09 21:31:11 +0900

    server

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
-const server = http.createServer(app); // HTTP 서버와 Socket.IO 서버를 합침
-const io = socketIo(server); // Socket.IO 서버 연결
+const server = http.createServer(app);
+const io = socketIo(server);
 
 const port = 3000;
 
-app.use(express.static('public')); // 클라이언트 파일을 제공할 public 폴더 설정
+// 루트 경로에서 index.html 제공
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // 클라이언트가 웹소켓을 통해 메시지를 보낼 때마다 처리
 io.on('connection', (socket) => {
   console.log('A user connected');
-  
+
   // 메시지를 받았을 때
   socket.on('chat message', (msg) => {
     console.log('Message received: ' + msg);
@@ -30,5 +33,5 @@ io.on('connection', (socket) => {
 // 서버 시작
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-  console.log("dokdox 를 이용해주셔서 대단히 감사합니다.작동이 검증되었습니다.")
+  console.log("dokdox 를 이용해주셔서 대단히 감사합니다. 작동이 검증되었습니다.");
 });
